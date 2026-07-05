53becdc45251e7c8b6cb3d56eaa37d3dbab17aff
murali-gude <gudemurali13@gmail.com>
2026-07-05 22:22:42 +0530

    initial

---
+++ src/infosysPractice/TraineeDetails.java
@@ -0,0 +1,10 @@
+package infosysPractice;
+
+public class TraineeDetails {
+	public static void main(String[]args) {
+		System.out.println("My name is Murali.");
+		System.out.println("My designation is Systems Engineer.");
+		
+	}
+
+}
