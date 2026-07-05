8c6aa474334840cbecc60a8fd300239da8093f69
murali-gude <gudemurali13@gmail.com>
2026-07-05 22:37:48 +0530

    Added new Java programs

---
+++ src/infosysPractice/AddressDetails.java
@@ -0,0 +1,11 @@
+package infosysPractice;
+
+public class AddressDetails {
+	public static void main(String[]args) {
+		System.out.println("Door No: D089\r\n"
+				+ "Street: St. Louis Street\r\n"
+				+ "City: Springfield\r\n"
+				+ "ZIP Code: 62729");
+	}
+
+}
