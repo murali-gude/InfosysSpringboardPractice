4b2191153ce596076de7e9dcd1f7ddf689009374
murali-gude <gudemurali13@gmail.com>
2026-07-06 23:24:30 +0530

    Added new Java programs

---
+++ src/infosysPractice/simpleInterest.java
@@ -0,0 +1,16 @@
+package infosysPractice;
+
+public class simpleInterest {
+	public static void main(String[]args) {
+		
+		int principal = 5000;
+		int roi = 10;
+		int time = 5;
+		
+		double simpleInterest = (principal*roi*time)/100;
+		
+		System.out.println(simpleInterest);
+			
+	}
+
+}
