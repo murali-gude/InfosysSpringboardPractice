79b465267e8bde974959a265c544f4ed910a3ab2
murali-gude <gudemurali13@gmail.com>
2026-07-08 22:25:58 +0530

    Added new Java programs

---
+++ src/infosysPractice/areaOfCircle.java
@@ -0,0 +1,15 @@
+package infosysPractice;
+
+public class areaOfCircle {
+	
+	public static void main(String[]args) {
+		
+		int r= 10 ;
+		
+		double areaOfCircle = (3.14* (r*r));
+		
+		System.out.println(areaOfCircle);
+		
+	}
+
+}
