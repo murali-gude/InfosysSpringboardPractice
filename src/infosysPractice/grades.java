fde681e92485e3cc6ab84fd84933d1bffe4e5337
murali-gude <gudemurali13@gmail.com>
2026-07-12 02:15:36 +0530

    Added new Java programs

---
+++ src/infosysPractice/grades.java
@@ -0,0 +1,27 @@
+package infosysPractice;
+
+public class grades {
+	
+	public static void main (String [] args) {
+		
+	
+	
+	int marks = 91;
+	
+	if(marks >=90) {
+		System.out.println(" A+");
+	}
+	else if (marks >= 80 && marks <90){
+		System.out.println("B");
+		
+	}
+	else if(marks >=70 && marks <80) {
+		System.out.println("C");
+		
+	}
+	else {
+		
+		System.out.println("Fail");
+	}
+
+}}
