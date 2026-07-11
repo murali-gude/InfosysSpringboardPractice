2e5f4cb0495b711dcf58b81be092f222ccb279be
murali-gude <gudemurali13@gmail.com>
2026-07-11 22:40:50 +0530

    Added new Java programs

---
+++ src/infosysPractice/fahrenheit2celcius.java
@@ -0,0 +1,14 @@
+package infosysPractice;
+
+public class fahrenheit2celcius {
+	
+	public static void main(String[]args) {
+		
+		int farh = 50;
+		
+		int c= ((farh-32)/9)*5;
+		
+		System.out.println(c);
+	}
+
+}
