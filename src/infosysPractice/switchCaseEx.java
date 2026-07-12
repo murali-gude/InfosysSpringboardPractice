4a1193d2c2b3095b45a529b647c62f01b104f4fe
murali-gude <gudemurali13@gmail.com>
2026-07-12 17:50:04 +0530

    Added new Java programs

---
+++ src/infosysPractice/switchCaseEx.java
@@ -0,0 +1,19 @@
+package infosysPractice;
+public class switchCaseEx {
+	public static void main(String[] args) {
+		String orderedFood = "Pizza";
+		switch (orderedFood) {
+		case "Burger":
+			System.out.println("You have ordered Burger. Unit price: $10");
+			break;
+		case "Pizza":
+			System.out.println("You have ordered Pizza. Unit price: $15");
+			break;
+		case "Sandwich":
+			System.out.println("You have ordered Sandwich. Unit price: $8");
+			break;
+		default:
+			System.out.println("Invalid selection");
+		}
+	}
+}
