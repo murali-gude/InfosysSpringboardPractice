eeb61503e160b2a1d1c3ecbf6d5f8b37716989b3
murali-gude <gudemurali13@gmail.com>
2026-07-13 22:46:12 +0530

    Added new Java programs

--- src/infosysPractice/whileloop.java
+++ src/infosysPractice/whileloop.java
@@ -1,13 +1,11 @@
 package infosysPractice;
 
-public class forLoop {
+public class whileloop {
 	public static void main(String[]args) {
-		int i=10;
-		
-		for(i=1; i <=20;i++) {
-			if(i%2==0) {
+		int i=1;
+		while(i<=10) {
 			System.out.println(i);
-			}
+			i++;
 		}
 	}
 
