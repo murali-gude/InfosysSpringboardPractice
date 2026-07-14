2379919ddc4d776c0686a4ded5ea569ed8c54808
murali-gude <gudemurali13@gmail.com>
2026-07-14 22:44:43 +0530

    Added new Java programs

---
+++ src/loops/factorial.java
@@ -0,0 +1,26 @@
+package loops;
+
+public class factorial {
+
+
+	    public static void main(String[] args) {
+
+	        int num=5;
+	        int fact =1;
+	        int i=1;
+	       
+	        
+	        do {
+	        	fact=fact*i;
+	        	i++;
+	        }
+	        while(i<=num) ;
+	        	
+	        	
+	        	
+	        
+
+	        System.out.println("Factorial = " + fact);
+	    }
+	}
+
