c9e181ec7cd3f78fa65ad70a0f178eb5fa4003b9
murali-gude <gudemurali13@gmail.com>
2026-07-14 23:21:31 +0530

    Added new Java programs

---
+++ src/loops/ZipZapZoom.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class ZipZapZoom {
+
+	public static void main(String[] args) {
+		int number = 15;
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("Zoom");
+        }
+        else if (number % 3 == 0) {
+            System.out.println("Zip");
+        }
+        else if (number % 5 == 0) {
+            System.out.println("Zap");
+        }
+        else {
+            System.out.println("Invalid");
+        }
+
+	}
+
+}
